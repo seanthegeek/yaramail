bb43cb56e72b888e98258ede81fc269c124cbb16
Sean Whalen <44679+seanthegeek@users.noreply.github.com>
2022-07-17 10:49:54 -0400

    Remove rstcheck from build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 . venv/bin/activate
-pip install -U -r requirements.txt && rstcheck --report-level warning README.rst && cd docs && make html && touch _build/html/.nojekyll && cp -rf _build/html/* ../../yaramail-docs/ && cd .. && flake8 yaramail  && rm -rf dist/ build/ && hatch
+pip install -U -r requirements.txt && cd docs && make html && touch _build/html/.nojekyll && cp -rf _build/html/* ../../yaramail-docs/ && cd .. && flake8 yaramail  && rm -rf dist/ build/ && hatch
