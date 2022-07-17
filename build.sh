5a8e3705fe18812a8590a3d04fb8b90eb05d2bd7
Sean Whalen <44679+seanthegeek@users.noreply.github.com>
2022-07-17 10:56:36 -0400

    Housekeeping

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 . venv/bin/activate
-pip install -U -r requirements.txt && cd docs && make html && touch _build/html/.nojekyll && cp -rf _build/html/* ../../yaramail-docs/ && cd .. && flake8 yaramail  && rm -rf dist/ build/ && hatch
+pip install -U -r requirements.txt && cd docs && make html && touch build/html/.nojekyll && cp -rf build/html/* ../../yaramail-docs/ && cd .. && flake8 yaramail  && rm -rf dist/ build/ && hatch build
