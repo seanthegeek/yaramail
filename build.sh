63beb365ad9c6ef304e0b4efb35cf11ed746eddc
Sean Whalen <seanthegeek@noreply.github.com>
2022-09-07 09:19:24 -0400

    Add tests to build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 . venv/bin/activate
-pip install -U -r requirements.txt && cd docs && make clean && make html && touch build/html/.nojekyll && cp -rf build/html/* ../../yaramail-docs/ && cd .. && flake8 yaramail  && rm -rf dist/ build/ && hatch build
+pip install -U -r requirements.txt && cd docs && make clean && make html && touch build/html/.nojekyll && cp -rf build/html/* ../../yaramail-docs/ && cd .. && flake8 yaramail  && python3 -m yaramail._cli -to test/samples  --rules test && rm -rf dist/ build/ && hatch build
