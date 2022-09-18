3df4c1abaf902f40fdba24ec4afb76eecc911dfb
Sean Whalen <44679+seanthegeek@users.noreply.github.com>
2022-09-17 21:16:05 -0400

    Update publish-docs.sh

--- publish-docs.sh
+++ publish-docs.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+cd ../yaramail-docs || exit
+git pull
+cd ../yaramail || exit
 ./build.sh
-cd ../yaramail-docs
+cd ../yaramail-docs || exit
 git add .
 git commit -m "Update docs"
 git push
