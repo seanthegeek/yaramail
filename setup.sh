3793b74d74d0fff7f5246f8c54d6cfefb08a63aa
Sean Whalen <seanthegeek@noreply.github.com>
2023-04-11 09:42:22 -0400

    Add openssl to setup.sh

--- setup.sh
+++ setup.sh
@@ -8,24 +8,24 @@ case "$OSTYPE" in
       echo 'Error: Homebrew is not installed. Please install it.' >&2
       exit 1
     fi
-    brew install pkg-config poppler python
+    brew install pkg-config openssl@3 poppler python
     ;;
 esac
 
 if [ -x "$(command -v apt)" ]; then
-  sudo apt install -y build-essential libpoppler-cpp-dev pkg-config python3-dev
+  sudo apt install -y build-essential libssl-dev libpoppler-cpp-dev pkg-config python3-dev
   if [ ! -x "$(command -v virtualenv)" ]; then
     sudo apt install -y python3-virtualenv
   fi
 fi
 
 if [ -x "$(command -v dnf)" ]; then
-  sudo dnf install gcc-c++ pkgconfig poppler-cpp-devel python3-devel
+  sudo dnf install gcc-c++ pkgconfig openssl-devel poppler-cpp-devel python3-devel
   if [ ! -x "$(command -v virtualenv)" ]; then
     sudo dnf install python3-virtualenv
   fi
   elif [ -x "$(command -v yum)" ]; then
-  sudo yum install gcc-c++ pkgconfig poppler-cpp-devel python3-devel
+  sudo yum install gcc-c++ pkgconfig openssl-devel poppler-cpp-devel python3-devel
     if [ ! -x "$(command -v virtualenv)" ]; then
       sudo yum install python3-virtualenv
     fi
