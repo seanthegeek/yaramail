0f0c05cc33630a8b5d6ddf366dae235563168f8e
Sean Whalen <44679+seanthegeek@users.noreply.github.com>
2023-04-13 10:39:17 -0400

    Update setup and build scripts

--- build.sh
+++ build.sh
@@ -7,6 +7,16 @@ if [ ! -d "venv" ]; then
 fi
 
 . venv/bin/activate
+case "$OSTYPE" in 
+  *darwin*)
+    if ! [ -x "$(command -v brew)" ]; then
+      echo 'Error: Homebrew is not installed. Please install it.' >&2
+      exit 1
+    fi
+    export LDFLAGS="-L/opt/homebrew/opt/openssl@3/lib"
+    export CPPFLAGS="-I/opt/homebrew/opt/openssl@3/include"
+    ;;
+esac
 pip install -U -r requirements.txt
 cd docs
 make clean 
--- setup.sh
+++ setup.sh
@@ -10,23 +10,22 @@ case "$OSTYPE" in
     fi
     brew install pkg-config openssl@3 poppler python
     ;;
-esac
-
-if [ -x "$(command -v apt)" ]; then
-  sudo apt install -y build-essential libssl-dev libpoppler-cpp-dev pkg-config python3-dev
-  if [ ! -x "$(command -v virtualenv)" ]; then
-    sudo apt install -y python3-virtualenv
-  fi
-fi
-
-if [ -x "$(command -v dnf)" ]; then
-  sudo dnf install gcc-c++ pkgconfig openssl-devel poppler-cpp-devel python3-devel
-  if [ ! -x "$(command -v virtualenv)" ]; then
-    sudo dnf install python3-virtualenv
-  fi
-  elif [ -x "$(command -v yum)" ]; then
-  sudo yum install gcc-c++ pkgconfig openssl-devel poppler-cpp-devel python3-devel
-    if [ ! -x "$(command -v virtualenv)" ]; then
-      sudo yum install python3-virtualenv
+  *linux*)
+    if [ -x "$(command -v apt)" ]; then
+      sudo apt install -y build-essential libssl-dev libpoppler-cpp-dev pkg-config python3-dev
+      if [ ! -x "$(command -v virtualenv)" ]; then
+        sudo apt install -y python3-virtualenv
+      fi
+    elif [ -x "$(command -v dnf)" ]; then
+      sudo dnf install gcc-c++ pkgconfig openssl-devel poppler-cpp-devel python3-devel
+      if [ ! -x "$(command -v virtualenv)" ]; then
+          sudo dnf install python3-virtualenv
+      fi
+    elif [ -x "$(command -v yum)" ]; then
+      sudo yum install gcc-c++ pkgconfig openssl-devel poppler-cpp-devel python3-devel
+      if [ ! -x "$(command -v virtualenv)" ]; then
+        sudo yum install python3-virtualenv
+      fi
     fi
-fi
+  ;;
+esac
